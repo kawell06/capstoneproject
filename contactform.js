4d919bdb38b75ee7ccbdbc5a37238219665f0010
Kaitlyn Wellendorff <kawell06@gmail.com>
2018-11-14 20:05:03 -0500

    Commit to Github

--- contactform.js
+++ contactform.js
@@ -1,4 +1,11 @@
-// Get HTML Elements 
+// Pop-Up Contact Form
+
+/*
+* By Clicking the Contact link a contact form will open
+* Click or tab through text fields to fill in details
+* Clicking submit will submit the form
+* Clicking cancel will close the contact form
+*/
 
 function openForm() {
 document.getElementById("contactForm").style.display="block";
